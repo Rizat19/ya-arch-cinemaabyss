8723d43c3bc80de0316833a06bc1216ca7651ba0
rorazalina <rorazalina@beeline.kz>
2025-07-09 12:25:18 +0500

    add new proxy-service

---
+++ src/microservices/proxy/src/test/java/ru/ri/proxy/ProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.ru.andreevcode.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
